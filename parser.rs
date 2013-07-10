5327f91776673d9052ef6ce45004f9c878105344
Simon Sapin <simon.sapin@exyr.org>
2013-07-10 17:30:43 +0100

    Relicense to MPL2

--- parser.rs
+++ parser.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // http://dev.w3.org/csswg/css-syntax/#parsing
 //
 // The input to the tree construction stage is a sequence of tokens
