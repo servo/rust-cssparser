4cdcd58a6325c355c7a24b7349ed04c6ad891497
Simon Sapin <simon.sapin@exyr.org>
2013-08-07 19:54:39 +0100

    Only accept variable (no complex expression) in the fake for loop.

--- parser.rs
+++ parser.rs
@@ -64,7 +64,7 @@ impl Iterator<(ComponentValue, SourceLocation)> for ComponentValueIterator {
 // Work around "error: cannot borrow `*iter` as mutable more than once at a time"
 // when using a normal for loop.
 macro_rules! for_iter(
-    ($iter: expr, $pattern: pat, $loop_body: expr) => (
+    ($iter: ident, $pattern: pat, $loop_body: expr) => (
         loop {
             match $iter.next() { None => break, Some($pattern) => $loop_body }
         }
