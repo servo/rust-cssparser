6dcd55fb70e3aba0645f7fa1a26054d6c0dd6b1c
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-18 00:43:12 +0100

    Add an easy way to toggle the procedural macro, so it's easier to test both behaviors.

--- build.rs
+++ build.rs
@@ -5,15 +5,33 @@ extern crate syn;
 use std::env;
 use std::path::Path;
 
+
+#[cfg(feature = "dummy_match_byte")]
+mod codegen {
+    use std::path::Path;
+    pub fn main(_: &Path) {}
+}
+
+#[cfg(not(feature = "dummy_match_byte"))]
 #[path = "src/macros/mod.rs"]
 mod macros;
 
+#[cfg(not(feature = "dummy_match_byte"))]
+mod codegen {
+    use macros;
+    use std::env;
+    use std::path::Path;
+
+    pub fn main(tokenizer_rs: &Path) {
+        macros::match_byte::expand(tokenizer_rs,
+                                   &Path::new(&env::var("OUT_DIR").unwrap()).join("tokenizer.rs"));
+
+    }
+}
+
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
     let tokenizer_rs = Path::new(&manifest_dir).join("src/tokenizer.rs");
-    macros::match_byte::expand(&tokenizer_rs,
-                               &Path::new(&env::var("OUT_DIR").unwrap()).join("tokenizer.rs"));
-
-     println!("cargo:rerun-if-changed={}", tokenizer_rs.display());
+    codegen::main(&tokenizer_rs);
+    println!("cargo:rerun-if-changed={}", tokenizer_rs.display());
 }
