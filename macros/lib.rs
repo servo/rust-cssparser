7f9e876cbee313c129c7ca4b1fd183e1a2e12693
Simon Sapin <simon.sapin@exyr.org>
2017-02-27 16:26:54 +0100

    Support full match syntax in match_ignore_ascii_case

--- macros/lib.rs
+++ macros/lib.rs
@@ -10,33 +10,58 @@ extern crate syn;
 use std::ascii::AsciiExt;
 
 /// Panic if any string contains ASCII uppercase letters.
-#[proc_macro_derive(cssparser__assert_ascii_lowercase)]
-pub fn assert_ascii_lowercase(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input(&input.to_string()).unwrap();
-
-    for token in find_smuggled_tokens(&input) {
-        let string = string_literal(token);
-        assert_eq!(*string, string.to_ascii_lowercase(),
-                   "the string patterns must be given in ASCII lowercase");
-    }
-
-    "".parse().unwrap()
+/// Emit a `MAX_LENGTH` constant with the length of the longest string.
+#[proc_macro_derive(cssparser__assert_ascii_lowercase__max_len)]
+pub fn assert_ascii_lowercase_max_len(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    max_len_common(input, |token_trees| {
+        let tokens = quote!( match x { #( #token_trees )* } );
+        let expr = syn::parse_expr(tokens.as_str()).unwrap();
+        let arms = match expr {
+            syn::Expr { node: syn::ExprKind::Match(_, ref arms), .. } => arms,
+            _ => panic!("expected a match expression, got {:?}", expr)
+        };
+        arms.iter().flat_map(|arm| &arm.pats).filter_map(|pattern| {
+            let expr = match *pattern {
+                syn::Pat::Lit(ref expr) => expr,
+                syn::Pat::Wild |
+                syn::Pat::Ident(_, _, None) => return None,
+                syn::Pat::Ident(_, _, Some(ref sub_pattern)) => {
+                    match **sub_pattern {
+                        syn::Pat::Lit(ref expr) => expr,
+                        syn::Pat::Wild => return None,
+                        _ => panic!("expected string or wildcard pattern, got {:?}", pattern)
+                    }
+                }
+                _ => panic!("expected string or wildcard pattern, got {:?}", pattern)
+            };
+            match **expr {
+                syn::Expr { node: syn::ExprKind::Lit(syn::Lit::Str(ref string, _)), .. } => {
+                    assert_eq!(*string, string.to_ascii_lowercase(),
+                               "string patterns must be given in ASCII lowercase");
+                    Some(string.len())
+                }
+                _ => panic!("expected string pattern, got {:?}", expr)
+            }
+        }).max()
+    })
 }
 
 /// Emit a `MAX_LENGTH` constant with the length of the longest string.
 #[proc_macro_derive(cssparser__max_len)]
 pub fn max_len(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input(&input.to_string()).unwrap();
+    max_len_common(input, |token_trees| {
+        token_trees.iter().map(|tt| string_literal(tt).len()).max()
+    })
+}
 
-    let token_trees = find_smuggled_tokens(&input);
-    let lengths = token_trees.iter().map(|tt| string_literal(tt).len());
-    let max_length = lengths.max().expect("expected at least one string");
-
-    let tokens = quote! {
-        const MAX_LENGTH: usize = #max_length;
-    };
-
-    tokens.as_str().parse().unwrap()
+fn max_len_common<F>(input: proc_macro::TokenStream, f: F) -> proc_macro::TokenStream
+where F : FnOnce(&[syn::TokenTree]) -> Option<usize> {
+    common(input, |token_trees| {
+        let max_length = f(token_trees).expect("expected at least one string");
+        quote! {
+            const MAX_LENGTH: usize = #max_length;
+        }
+    })
 }
 
 /// ```
@@ -46,29 +71,35 @@ pub fn max_len(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 /// Map keys are ASCII-lowercased.
 #[proc_macro_derive(cssparser__phf_map)]
 pub fn phf_map(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    common(input, |token_trees| {
+        let value_type = &token_trees[0];
+        let pairs: Vec<_> = token_trees[1..].chunks(2).map(|chunk| {
+            let key = string_literal(&chunk[0]);
+            let value = &chunk[1];
+            (key.to_ascii_lowercase(), quote!(#value).to_string())
+        }).collect();
+
+        let mut map = phf_codegen::Map::new();
+        for &(ref key, ref value) in &pairs {
+            map.entry(&**key, &**value);
+        }
+
+        let mut tokens = quote! {
+            static MAP: ::phf::Map<&'static str, #value_type> =
+        };
+        let mut initializer_bytes = Vec::new();
+        map.build(&mut initializer_bytes).unwrap();
+        tokens.append(::std::str::from_utf8(&initializer_bytes).unwrap());
+        tokens.append(";");
+        tokens
+    })
+}
+
+fn common<F>(input: proc_macro::TokenStream, f: F) -> proc_macro::TokenStream
+where F: FnOnce(&[syn::TokenTree]) -> quote::Tokens {
     let input = syn::parse_macro_input(&input.to_string()).unwrap();
-
     let token_trees = find_smuggled_tokens(&input);
-    let value_type = &token_trees[0];
-    let pairs: Vec<_> = token_trees[1..].chunks(2).map(|chunk| {
-        let key = string_literal(&chunk[0]);
-        let value = &chunk[1];
-        (key.to_ascii_lowercase(), quote!(#value).to_string())
-    }).collect();
-
-    let mut map = phf_codegen::Map::new();
-    for &(ref key, ref value) in &pairs {
-        map.entry(&**key, &**value);
-    }
-
-    let mut tokens = quote! {
-        static MAP: ::phf::Map<&'static str, #value_type> =
-    };
-    let mut initializer_bytes = Vec::new();
-    map.build(&mut initializer_bytes).unwrap();
-    tokens.append(::std::str::from_utf8(&initializer_bytes).unwrap());
-    tokens.append(";");
-
+    let tokens = f(token_trees);
     tokens.as_str().parse().unwrap()
 }
 
