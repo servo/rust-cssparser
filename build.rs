67d2dd98688d6fe6d83a68fb2bef32bdf2cff24e
Simon Sapin <simon.sapin@exyr.org>
2017-11-21 09:28:02 +0100

    Upgrade to rustc 1.23.0-nightly (33374fa9d 2017-11-20)

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@
 
 extern crate quote;
 extern crate syn;
+extern crate rustc_version;
 
 use std::env;
 use std::path::Path;
@@ -33,6 +34,18 @@ mod codegen {
 }
 
 fn main() {
+    let rustc = rustc_version::version_meta().unwrap();
+    let commit_date = match rustc.commit_date {
+        Some(ref s) => &**s,
+        None => "unknown",  // Sorts after a date
+    };
+    if (rustc.semver.major, rustc.semver.minor, rustc.semver.patch, commit_date) >=
+       (1, 23, 0, "2017-11-20")
+    {
+        // https://github.com/rust-lang/rust/pull/45225
+        println!("cargo:rustc-cfg=rustc_has_pr45225")
+    }
+
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let tokenizer_rs = Path::new(&manifest_dir).join("src/tokenizer.rs");
     codegen::main(&tokenizer_rs);
