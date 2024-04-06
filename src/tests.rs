ead3a4cc1b0fe001b3039825bd19bfdabfecbbde
Chipnertkj <32001812+chipnertkj@users.noreply.github.com>
2024-04-06 04:59:08 +0200

    Revert "Typo in function call; see earlier function name change commit"
    This reverts commit 563b1946a89e75c0e1ac3ec2842708434492c0ee.

--- src/tests.rs
+++ src/tests.rs
@@ -823,7 +823,7 @@ fn numeric(b: &mut Bencher) {
         for _ in 0..1000000 {
             let mut input = ParserInput::new("10px");
             let mut input = Parser::new(&mut input);
-            let _ = test::black_box(input.try_next());
+            let _ = test::black_box(input.next());
         }
     })
 }
